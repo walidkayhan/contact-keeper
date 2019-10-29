e7bedad6ce93ebfe1a7fa649cabac5d6598d282b
Walid <walid.kayhan@mohawkcollege.ca>
2019-10-29 18:20:40 -0400

    Implemented components

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,49 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+
 import Navbar from './components/layout/Navbar';
+import Alerts from './components/layout/Alerts';
+
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
 
 const App = () => {
 	return (
-		<ContactState>
-			<Router>
-				<Fragment className="App">
-					<Navbar />
-					<div className="container">
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route exact path="/about" component={About} />
-						</Switch>
-					</div>
-				</Fragment>
-			</Router>
-		</ContactState>
+		<AuthState>
+			<ContactState>
+				<AlertState>
+					<Router>
+						<div className="App">
+							<Navbar />
+							<div className="container">
+								<Alerts />
+
+								<Switch>
+									<PrivateRoute exact path="/" component={Home} />
+									<Route exact path="/about" component={About} />
+									<Route exact path="/register" component={Register} />
+									<Route exact path="/login" component={Login} />
+								</Switch>
+							</div>
+						</div>
+					</Router>
+				</AlertState>
+			</ContactState>
+		</AuthState>
 	);
 };
 
---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,48 @@
+import React, { useContext, Fragment, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import ContactContext from '../../context/contact/contactContext';
+import ContactItem from './ContactItem';
+import Spinner from '../layout/Spinner';
+
+const Contacts = () => {
+	const contactContext = useContext(ContactContext);
+
+	const { contacts, filtered, getContacts, loading } = contactContext;
+
+	useEffect(() => {
+		getContacts();
+		// eslint-disable-next-lin
+	}, []);
+
+	if (contacts !== null && contacts.length === 0 && !loading) {
+		return <h3>Please add a contact</h3>;
+	}
+
+	return (
+		<div>
+			<Fragment>
+				{contacts !== null && !loading ? (
+					<TransitionGroup>
+						{filtered !== null ? (
+							filtered.map((contact) => (
+								<CSSTransition key={contact._id} timeout={500} classNames="item">
+									<ContactItem contact={contact} />
+								</CSSTransition>
+							))
+						) : (
+							contacts.map((contact) => (
+								<CSSTransition key={contact._id} timeout={500} classNames="item">
+									<ContactItem contact={contact} />
+								</CSSTransition>
+							))
+						)}
+					</TransitionGroup>
+				) : (
+					<Spinner />
+				)}
+			</Fragment>
+		</div>
+	);
+};
+
+export default Contacts;
