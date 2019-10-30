76b10a1ce5496c653e37bd88bd8ae81f088f39e4
Walid <walid.kayhan@mohawkcollege.ca>
2019-10-29 23:40:08 -0400

    Removed un-needed route

--- server.js
+++ server.js
@@ -22,11 +22,6 @@ if (process.env.NODE_ENV === 'production') {
 	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client/build/index.html')));
 }
 
-//Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
